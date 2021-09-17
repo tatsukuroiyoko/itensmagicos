459219ec759984283498521c241e4172e85ce0f4
tatsukuroiyoko <tatsukuroiyoko@gmail.com>
2021-09-17 01:54:33 -0300

    versao 0.0.1

---
+++ gerador0.0.01.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+int main (void)
+
+{
+	std::cout << " Informe tipo do item: " << '\n';
+
+	
+	int nivelConjurador;
+	int nivelMagia;
+	
+				
+	std::cout << "Nivel do Conjurador:";
+	std::cin >> nivelConjurador;
+	std::cout << "Nivel da Magia:";
+	std::cin >> nivelMagia;
+	
+	float precoBase = (nivelMagia*nivelConjurador)*25;
+	float materiaPrima = precoBase/2;	
+	float custoXP = precoBase/25;
+	float tempoProd = precoBase/125;
+	
+	std::cout << " Custo base: " << precoBase << '\n';
+	std::cout << " Custo Materia Prima: " << materiaPrima << '\n';
+	std::cout << " Custo XP: " << custoXP << '\n';
+	
+	std::cout << std::fixed << std::setprecision(1);
+	std::cout << " Horas para Criar: " << tempoProd << '\n'; 
+		
+	return 0;
+}
